0ffef8d58745678ec3f0afd4daf372d1f433c43f
Hans Evan Tatipata <thansevan@gmail.com>
2023-09-25 21:57:06 +0700

    package and feature section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,29 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Image from '../assets/img/Image.png'
 import { FiPlayCircle } from 'react-icons/fi';
+import PackageCard from '../components/HomePage/PackageCard';
+import FeatureCard from '../components/HomePage/FeatureCard';
 
 const Home = () => {
+
+  const features = [
+    { id: 1, text: 'Aman', desc : "Dikelola oleh manajemen Bank BRI, serta seluruh transaksi dilakukan melalui overbooking untuk meningkatkan keamanan transaksi."},
+    { id: 2, text: 'Akses Luas', desc : "Jaringan kerja konvensional maupun e-channel BRI tersebar di seluruh Indonesia, memudahkan peserta DPLK BRI dalam bertransaksi."},
+    { id: 3, text: 'Investasi Beragam dan Prudent', desc:  "DPLK BRI mengutamakan prinsip kehati-hatian dalam penempatan dana dengan 6 pilihan investasi"},
+    { id: 4, text: 'Return Optimal', desc: "Secara historis, DPLK BRI memberikan return yang baik."},
+    { id: 5, text: 'Pengelolaan Modern', desc: "Pengelolaan investasi DPLK BRI memakai konsep pengelolaan reksadana yang dihitung harian dan dikirim secara periodik"},
+    { id: 6, text: 'Berpengalaman', desc: "Pengalaman bekerjasama dengan lebih dari 200 korporasi terkemuka di Indonesia"},
+  ];
+
+  const packages = [
+    { id: 1, text: 'DPLK BRI Pasar uang', points : ["Efek Pasar uang: 100%"] },
+    { id: 2, text: 'DPLK BRI Pendapatan Tetap', points : ["Efek Pendapatan Tetap: 60 - 100%", "Efek Pasar Uang: 0 - 40%"] },
+    { id: 3, text: 'DPLK BRI Saham', points: ["Efek Saham: 60 - 100%","Efek Pasar Uang: 0 - 40%"] },
+    { id: 4, text: 'DPLK BRI Pasar Uang Syariah', points: ["Efek Pasar Uang Syariah : 100%"] },
+    { id: 5, text: 'DPLK BRI Berimbang Syariah', points: ["Efek Pasar Uang Syariah : 0 -100%","Efek Pendapatan Tetap Syariah : 0 - 50%", "Efek Saham Syariah : 0 - 50%"] },
+    { id: 6, text: 'DPLK BRI Kombinasi', points: ["Gabungan antara konvensional dan syariah."] },
+  ];
+
   return (
     <>
         <Navbar />
@@ -24,7 +45,46 @@ const Home = () => {
             </div>
             
           </div>
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="201" viewBox="0 0 1440 201" fill="none">
+<path d="M0 0C96 30.8 288 142.2 480 154C672 165.8 768 48.6 960 59C1152 69.4 1344 176.6 1440 206V281H0V0Z" fill="#014A94"/>
+</svg>
         </section>
+
+        
+      <section className="mb-32">
+      <div className="container my-24 mx-auto md:px-6">
+        <h5 className="text-center text-sky-800 text-sm font-bold font-['DM Sans'] uppercase leading-tight tracking-widest">
+        Features
+        </h5>
+        <h2 className="text-center text-gray-700 text-3xl font-bold font-['DM Sans'] leading-10">Keuntungan DPLK BRI
+        </h2>
+        <div className="grid gap-x-6 md:grid-cols-3 lg:gap-x-12 gap-y-16 mt-20">
+
+        {features.map((feature)=>(
+          <FeatureCard key={feature.id} text={feature.text} desc={feature.desc} />
+        ))}
+
+        </div>
+        
+    </div>
+      </section>
+      <section className="mb-32">
+      <div className="container my-24 mx-auto md:px-6">
+        <h5 className="text-center text-sky-800 text-sm font-bold font-['DM Sans'] uppercase leading-tight tracking-widest">
+        Package
+        </h5>
+        <h2 className="text-center text-gray-700 text-3xl font-bold font-['DM Sans'] leading-10">Paket Pilihan Investasi
+        </h2>
+        <div className="grid gap-x-6 md:grid-cols-3 lg:gap-x-12 gap-y-16 mt-20">
+
+        {packages.map((pack)=>(
+          <PackageCard key={pack.id} title={pack.text} points={pack.points} />
+        ))}
+
+        </div>
+        
+    </div>
+      </section>
     </>
   )
 }
