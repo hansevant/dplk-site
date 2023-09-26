5fbaddbd0440401951b56a9bf42423a57458fed2
Hans Evan Tatipata <thansevan@gmail.com>
2023-09-27 02:44:14 +0700

    finishing homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Navbar from '../components/Navbar';
 import Image from '../assets/img/Image.png'
+import Phone from '../assets/img/Phone.png'
+import AppStore from '../assets/img/app_store.png'
+import GooglePlay from '../assets/img/google_play.png'
 import { FiPlayCircle } from 'react-icons/fi';
 import PackageCard from '../components/HomePage/PackageCard';
 import FeatureCard from '../components/HomePage/FeatureCard';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"
+import '../assets/css/Slider.css'
+import SliderCard from '../components/HomePage/SliderCard';
 
 const Home = () => {
+
+  
 
   const features = [
     { id: 1, text: 'Aman', desc : "Dikelola oleh manajemen Bank BRI, serta seluruh transaksi dilakukan melalui overbooking untuk meningkatkan keamanan transaksi."},
@@ -25,6 +34,33 @@ const Home = () => {
     { id: 6, text: 'DPLK BRI Kombinasi', points: ["Gabungan antara konvensional dan syariah."] },
   ];
 
+  const testimonials = [
+    {id: 1, name:"Prisca Yuliana", star: 5},
+    {id: 2, name:"Fachrie Malyan", star: 4},
+    {id: 3, name:"Ernita Deliami", star: 5},
+    {id: 4, name:"Abizar Ramadhan", star: 3},
+  ]
+
+    const sliderRef = useRef(null);
+  
+    const next = () => {
+      sliderRef.current.slickNext();
+    };
+  
+    const previous = () => {
+      sliderRef.current.slickPrev();
+    };
+  
+
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: null, // Set to null to hide the previous arrow
+    nextArrow: null, // Set to null to hide the next arrow
+  };
+
   return (
     <>
         <Navbar />
@@ -34,8 +70,8 @@ const Home = () => {
             <div className="lg:col-span-5">
               <h1 className="text-gray-700 text-5xl font-bold font-['DM Sans'] leading-10">Selamat Datang di DPLK BRI</h1>
               <p className="text-gray-700 text-lg font-normal font-['DM Sans'] mt-6 leading-relaxed">BRI DPLK | Dana Pensiun Lembaga Keuangan Bank Rakyat Indonesia (DPLK BRI) menyelenggarakan Program Pensiun Iuran Pasti (PPIP) bagi peserta Individu maupun Korporasi, serta Program Pensiun Untuk Kompensasi Pesangon (PPUKP) bagi peserta Korporasi.</p>
-              <a href="##" className="text-center text-gray-700 text-lg font-semibold font-['Inter'] leading-10 mt-6 inline-flex items-center px-5 py-1 rounded-lg border border-gray-700">
-                <FiPlayCircle size={16} style={{ color: 'rgb(55 65 81)' }}/>
+              <a href="https://www.youtube.com/watch?v=X9eFHmCsQ2Y&pp=ygUIZHBsayBicmk%3D" className="text-center text-gray-700 text-lg font-semibold font-['Inter'] leading-10 mt-6 inline-flex items-center px-5 py-1 rounded-lg border hover:bg-gray-200 transition border-gray-700">
+                <FiPlayCircle size={20} style={{ color: 'rgb(55 65 81)' }}/>
                 <p className='ml-2'>Tonton Video</p>
               </a>
             </div>
@@ -46,45 +82,101 @@ const Home = () => {
             
           </div>
           <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="201" viewBox="0 0 1440 201" fill="none">
-<path d="M0 0C96 30.8 288 142.2 480 154C672 165.8 768 48.6 960 59C1152 69.4 1344 176.6 1440 206V281H0V0Z" fill="#014A94"/>
-</svg>
+            <path d="M0 0C96 30.8 288 142.2 480 154C672 165.8 768 48.6 960 59C1152 69.4 1344 176.6 1440 206V281H0V0Z" fill="#014A94"/>
+          </svg>
         </section>
 
         
       <section className="mb-32">
-      <div className="container my-24 mx-auto md:px-6">
-        <h5 className="text-center text-sky-800 text-sm font-bold font-['DM Sans'] uppercase leading-tight tracking-widest">
-        Features
-        </h5>
-        <h2 className="text-center text-gray-700 text-3xl font-bold font-['DM Sans'] leading-10">Keuntungan DPLK BRI
-        </h2>
-        <div className="grid gap-x-6 md:grid-cols-3 lg:gap-x-12 gap-y-16 mt-20">
+      <div className="container my-12 mx-auto md:px-6">
+          <h5 className="text-center text-sky-800 text-sm font-bold font-['DM Sans'] uppercase leading-tight tracking-widest">
+          Features
+          </h5>
+          <h2 className="text-center text-gray-700 text-3xl font-bold font-['DM Sans'] leading-10">Keuntungan DPLK BRI
+          </h2>
 
-        {features.map((feature)=>(
-          <FeatureCard key={feature.id} text={feature.text} desc={feature.desc} />
-        ))}
+          <div className="grid gap-x-6 md:grid-cols-3 lg:gap-x-12 gap-y-16 mt-20">
 
+            {features.map((feature)=>(
+              <FeatureCard key={feature.id} text={feature.text} desc={feature.desc} />
+            ))}
+
+          </div>
         </div>
-        
-    </div>
       </section>
       <section className="mb-32">
-      <div className="container my-24 mx-auto md:px-6">
-        <h5 className="text-center text-sky-800 text-sm font-bold font-['DM Sans'] uppercase leading-tight tracking-widest">
-        Package
-        </h5>
-        <h2 className="text-center text-gray-700 text-3xl font-bold font-['DM Sans'] leading-10">Paket Pilihan Investasi
-        </h2>
-        <div className="grid gap-x-6 md:grid-cols-3 lg:gap-x-12 gap-y-16 mt-20">
+        <div className="container my-12 mx-auto md:px-6">
+            <h5 className="text-center text-sky-800 text-sm font-bold font-['DM Sans'] uppercase leading-tight tracking-widest">
+            Package
+            </h5>
+            <h2 className="text-center text-gray-700 text-3xl font-bold font-['DM Sans'] leading-10">Paket Pilihan Investasi
+            </h2>
+            <div className="grid gap-x-6 md:grid-cols-3 lg:gap-x-12 mt-20">
 
-        {packages.map((pack)=>(
-          <PackageCard key={pack.id} title={pack.text} points={pack.points} />
-        ))}
+            {packages.map((pack)=>(
+              <PackageCard key={pack.id} title={pack.text} points={pack.points} />
+            ))}
+
+            </div>
+            
+        </div>
+      </section>
+      <section className="mb-16">
+        <div className="container my-12 mx-auto md:px-6">
+            <h5 className="text-center text-sky-800 text-sm font-bold font-['DM Sans'] uppercase leading-tight tracking-widest">
+            Testimonial
+            </h5>
+            <h2 className="text-center text-gray-700 text-3xl font-bold font-['DM Sans'] leading-10">
+            Dengarkan Cerita Mereka
+            </h2>
+
+            <div className="slider-container text-center">
+
+              <Slider ref={sliderRef} {...settings}>
+                {testimonials.map((testi) => (
+                  <SliderCard key={testi.id} name={testi.name} star={testi.star} />
+                ))}
+              </Slider>
+
+              <div className="slider-controls mt-4 flex gap-2 justify-center">
+                <button className="slider-button bg-blue-500 text-white py-2 px-4 rounded" onClick={previous}>
+                  Previous
+                </button>
+                <button className="slider-button bg-blue-500 text-white py-2 px-4 rounded" onClick={next}>
+                  Next
+                </button>
+              </div>
+            </div>
 
         </div>
-        
-    </div>
       </section>
+
+      <section className="bg-white mb-16">
+          <div className="max-w-screen-lg px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:flex justify-between items-center">
+
+          <div className="hidden lg:block">
+            <img src={Phone} alt="mockup" style={{ width: '725px', height: 'auto' }} />
+          </div>
+          <div className="lg:col-span-5">
+            <h2 className="text-sky-800 text-sm font-bold font-['DM Sans'] uppercase leading-tight tracking-widest">DPLK BRIMO</h2>
+            <h1 className="text-gray-700 text-5xl font-bold font-['DM Sans'] mt-3 leading-10">Kini DPLK BRI tersedia di BRIMO</h1>
+            <p className="text-gray-700 text-lg font-normal font-['DM Sans'] mt-6 leading-relaxed">Segera daftarkan ke unit kerja BRI terdekat dan dapatkan di Apps Store dan Play Store.</p>
+            <div className="mt-10">
+              <a href='https://play.google.com/store/apps/details?id=id.co.bri.brimo&pcampaignid=web_share'>
+                <img src={GooglePlay} alt="mockup" style={{ width: 'auto', height: 'auto' }} />
+              </a>
+              <a href='https://apps.apple.com/id/app/brimo-bri/id1439730817'>
+                <img src={AppStore} alt="mockup" style={{ width: 'auto', height: 'auto' }} />
+              </a>
+            </div>
+          </div>
+            
+            
+          </div>
+        </section>
+        <div className="w-full py-4 bg-sky-800 justify-center items-center inline-flex">
+          <p className="text-center text-white text-base font-normal font-['DM Sans'] leading-relaxed">© 2023 PT.Bank Rakyat Indonesia (Persero) Tbk. | All Rights Reserved.</p>
+        </div>
     </>
   )
 }
