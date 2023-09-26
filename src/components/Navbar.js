2a238cd4471a0ad4f15dfc15b61bf4cb3c7029a2
Hans Evan Tatipata <thansevan@gmail.com>
2023-09-27 03:29:45 +0700

    detailing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,14 @@ const Navbar = () => {
 
   return (
     <div className='container max-w-5xl mx-auto flex flex-row justify-between py-5 px-6 items-center'>
-        <img alt='logo' src={LogoNav} className='w-36' />
-            <ul className='flex gap-5 text-gray-700 text-base font-medium leading-snug'>
-                {menu.map((value, i) => (
-                    <li key={i}><a href='##' className='hover:text-[#014A94]'>{value}</a></li>
-                ))}
-            </ul>
+        <a href='/'>
+            <img alt='logo' src={LogoNav} className='w-36' />
+        </a>
+        <ul className='flex gap-5 text-gray-700 text-base font-medium leading-snug'>
+            {menu.map((value, i) => (
+                <li key={i}><a href='soon' className='hover:text-[#014A94]'>{value}</a></li>
+            ))}
+        </ul>
         <div>
             <a href='/login' className='text-lg bg-gray-100 text-blue-800 font-semibold rounded-md px-7 py-2 hover:bg-blue-100 transition'>
             Masuk
--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -9,6 +9,9 @@ function ComingSoon() {
       <p className="text-white text-lg mb-8">
         We're working hard to bring you something amazing. Stay tuned!
       </p>
+      <a class="px-6 py-2 font-semibold rounded-md bg-slate-600 text-white" href='/'>
+          Back to Home
+      </a>
     </div>
   );
 }
